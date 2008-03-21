d610025e36481b06964ef48ec6b6e9aa10f04ffc
Steve Hoeksema <steve@music.seven.net.nz>
2008-03-21 16:59:04 +1200

    first commit

---
+++ gigs.rb
@@ -0,0 +1,89 @@
+require 'rubygems'
+require 'hpricot'
+require 'rss/maker'
+require 'ramaze'
+
+class Gig
+
+  attr_accessor :url, :title, :tags, :date, :location, :price
+  
+end
+
+class GigGuide
+
+  attr_reader :gigs
+
+  def initialise
+  
+    doc = Hpricot(File.open("index.htm"))
+
+    @gigs = []
+    
+    (doc/"div#mainbox2 tr.vevent").each do |gigrow|
+    
+      begin
+      
+        gig = Gig.new
+        
+        col1 = (gigrow/"td:nth-child(0)")
+        gig.url = "http://www.mukuna.co.nz" + (col1/"a.url").attr("href")
+        gig.title = (col1/"a.url").inner_html
+        gig.tags = (col1/"span.description").inner_html
+        gig.date = Time.parse((col1/"abbr.dtstart").attr("title"))
+        
+        location_pieces = (gigrow/"td:nth-child(1)").search("*")
+        gig.location = [location_pieces[2], location_pieces[4]].join(", ")
+        
+        gig.price = (gigrow/"td:nth-child(2)").inner_html
+        
+        @gigs << gig
+      
+      # ignore malformed rows
+      rescue NoMethodError
+      
+      end
+      
+    end
+  
+  end
+  
+  def rss
+  
+    RSS::Maker.make("2.0") do |maker|
+
+      maker.channel.about = "Auckland Gig Guide"
+      maker.channel.title = "Mukuna"
+      maker.channel.description = "Mukuna Auckland Gig Guide"
+      maker.channel.link = "http://www.mukuna.co.nz/"
+      
+      maker.image.title = "Mukuna Logo"
+      maker.image.url = "http://www.mukuna.co.nz/img/mukuna.gif"
+    
+      for gig in @gigs
+        maker.items.new_item do |item|
+          item.link = gig.url
+          item.title = gig.title + " at " + gig.location
+          item.date = gig.date
+          item.description = gig.price + ", " + gig.tags
+        end
+      end
+      
+    end
+
+  end
+
+end
+ 
+class MainController < Ramaze::Controller
+
+  def index
+  
+    guide = new GigGuide
+    
+    guide.rss
+    
+  end
+  
+end
+
+Ramaze.start :adapter => :mongrel, :port => 7000
